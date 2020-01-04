63ad494289562ee8c0848aacb68951b58a80a4e5
travis skyles <travis.skyles@gmail.com>
2020-01-03 16:18:26 -0800

    add test for uRl

--- __tests__/filterByTerm.spec.js
+++ __tests__/filterByTerm.spec.js
@@ -13,4 +13,18 @@ describe('Filter Function', () => {
 		expect(filterByTerm(input, 'link')).toEqual(output);
 		expect(filterByTerm(input, 'Link')).toEqual(output);
 	});
+	test('It should filter by a search term (uRl)', () => {
+		const input = [
+			{ id: 1, url: 'https://www.url1.dev' },
+			{ id: 2, url: 'https://www.url2.dev' },
+			{ id: 3, url: 'https://www.link3.dev' }
+		];
+
+		const output = [
+			{ id: 1, url: 'https://www.url1.dev' },
+			{ id: 2, url: 'https://www.url2.dev' }
+		];
+
+		expect(filterByTerm(input, 'uRl')).toEqual(output);
+	});
 });
